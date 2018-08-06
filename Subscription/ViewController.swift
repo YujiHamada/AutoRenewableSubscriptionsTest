31c3eb484309aeea3ae13330e30e0d641f66a9c7
濱田裕史 <uziu2hahaha@gmail.com>
2018-08-06 21:01:54 +0900

    Initial Commit

---
+++ Subscription/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Subscription
+//
+//  Created by 濱田裕史 on 2018/08/06.
+//  Copyright © 2018年 濱田裕史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
